4b17d247b25d929ce4fe84c79b09a7e16844f3af
AsmaRomany <1180989@student.birzeit.edu>
2023-06-04 23:16:20 +0300

    Asma

--- app/src/main/java/com/example/asmaapplication/MainActivity.java
+++ app/src/main/java/com/example/asmaapplication/MainActivity.java
@@ -1,8 +1,20 @@
 package com.example.asmaapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       final DrawerLayout drawerLayout= findViewById(R.id.drawerlayout);
+       findViewById(R.id.image1).setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               drawerLayout.openDrawer(GravityCompat.START);
+           }
+       });
+        NavigationView navigationView= findViewById(R.id.nav);
+        navigationView.setItemIconTintList(null);
+        NavController navController= Navigation.findNavController(this,R.id.fragment);
+        NavigationUI.setupWithNavController(navigationView,navController);
+        final TextView Titel=findViewById(R.id.text);
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController navController, @NonNull NavDestination navDestination, @Nullable Bundle bundle) {
+                Titel.setText(navDestination.getLabel());
+            }
+        });
     }
 }
